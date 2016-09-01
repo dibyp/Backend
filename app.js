ce4830c8d9f0e18435207a3595b54618e173b807
류태선 <dibyp@lyutaeseon-ui-MacBook-Pro.local>
2016-09-01 16:38:09 +0900

    라우터 분리

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var http = require('http');
+var router = require('./router.js');
+var url = require('url');
+
+var app = http.createServer(function(req,res){
+	var query = url.parse(req.url, true).query;
+
+	console.log(req.url);
 
 
-http.createServer(funciton(req,res){
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.write("hello world \n");
-	res.end();
-}).listen(3030);
+	router.home(req, res);
+	router.detail(req, res);
 
-console.log("Server Running at localhost:3030");
+});
+
+app.listen(3030, function(){
+	console.log("Server Running at localhost:3030");
+});
+
